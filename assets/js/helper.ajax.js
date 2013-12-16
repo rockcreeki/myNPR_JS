6ab3fecf4108bc8932360ce93d1fb78bb5b5f456
Joe Rodriguez <rockcreekinteractive@gmail.com>
2013-12-15 23:05:59 -0800

    Refactoring
    Eliminated some redundancies in the AJAX helper file; shifted some of
    the setup to the main app module

--- assets/js/helper.ajax.js
+++ assets/js/helper.ajax.js
@@ -9,77 +9,26 @@ $.ajaxSetup({
   }  
 });
 
-/*** Topics ***/
-function requestTopics(){
-  console.info('request topics');
-  return $.ajax(as.uri.list,{
-      data: {
-          id : 3002   
-      },
-      success: requestTopicsSuccess,
-      error: requestTopicsError
-  });  
-}
-
-function requestTopicsSuccess(data, status, xhr){
-  console.info('request topics success');
-}
-
-function requestTopicsError(xhr, status, error){
-  console.info('request topics error');
-  $('#content').text('An error occurred: ' + error + ': ' + status);    
-}
-
-/*** Tags ***/
-function requestTags(){
-  console.info('request tags');
-  return $.ajax(as.uri.list,{
-      data: {
-          id : 3024
-      },
-      success: requestTagsSuccess,
-      error: requestTagsError,
-      cache: true 
-  });  
-}
-
-function requestTagsSuccess(data, status, xhr){
-  console.info('request tags success');
-}
-
-function requestTagsError(xhr, status, error){
-  console.info('request tags error');
-  $('#content').text('An error occurred: ' + error + ': ' + status);    
-}
-
-/*** GENRES ***/
-function requestGenres(){
-  console.info('request genres');
-  return $.ajax(as.uri.list,{
-      data: {
-          id : 3018   
-      },
-      success: requestGenresSuccess,
-      error: requestGenresError
-  });  
-}
-
-function requestGenresSuccess(data, status, xhr){
-  console.info('request genres success');
-}
-
-function requestGenresError(xhr, status, error){
-  console.info('request genres error');
-  $('#content').text('An error occurred: ' + error + ': ' + status);    
-}
-
-
 (function(async){
-  as = async;
-  as.uri = {
-      list: 'http://api.npr.org/list'
-  };
-  as.requestTopics = requestTopics;
-  as.requestGenres = requestGenres;
-  as.requestTags = requestTags;
+  
+  function requestList(options){
+    console.info('request ' + options.type);
+    return $.ajax(options.uri,{
+        data: options.data,
+        context: options,
+        success: requestListSuccess,
+        error: requestListError
+    });  
+  }
+  
+  function requestListSuccess(data, status, xhr){
+    console.info('request ' + this.type + ' list success');
+  }
+  
+  function requestListError(xhr, status, error){
+    console.info('request ' + this.type + ' list error');
+    $('#content').text('An error occurred: ' + error + ': ' + status);    
+  }
+  
+  async.requestList = requestList;
 })(AJAX || (AJAX = {}));
--- assets/js/script.js
+++ assets/js/script.js
@@ -4,77 +4,114 @@ $(document).ready(function(e){
   $('#topics').on('click', myNPR.showTopics);
   $('#tags').attr('disabled','disabled').on('click', myNPR.showTags);
   $('#genres').on('click', myNPR.showGenres); 
-  loadTopicsAndTags();
-  //loadTopicsAndGenres();
-})
-
-function loadTopicsAndTags(){
-  $.when(AJAX.requestTopics(), AJAX.requestTags())
-    .then(function(topics, tags){
-      myNPR.topics = topics[0];
-      myNPR.tags = tags[0];
-      // Over 17k tags so it takes a while to load  =)
-      // The tags button is disabled until they're done loading
-      $('#tags').removeAttr('disabled');
-    });  
-}
-
-function loadTopicsAndGenres(){
-  $.when(AJAX.requestTopics(), AJAX.requestGenres())
-    .then(function(topics, genres){
-      myNPR.topics = topics[0];
-      myNPR.genres = genres[0];
-    });  
-}
-
-function showTopics(){
-  console.info('show topics');
-  if(!myNPR.topics)
-  {
-    $.when(AJAX.requestTopics())
-      .then(function(data){myNPR.topics = data; updateContentDiv(myNPR.topics);});
-  }
-  else {
-    updateContentDiv(myNPR.topics);
-  }
-}
-
-function showTags(){
-  console.info('show tags');
-  if(!myNPR.tags)
-  {
-    $.when(AJAX.requestTags())
-      .then(function(data){myNPR.tags = data; updateContentDiv(myNPR.tags);});
-  }
-  else {
-    updateContentDiv(myNPR.tags);
-  }
-}
-
-function showGenres(){
-  console.info('show genres');
-  if(!myNPR.genres)
-  {
-    $.when(AJAX.requestGenres())
-      .then(function(data){myNPR.genres = data; updateContentDiv(myNPR.genres)});
-  }
-  else {
-    updateContentDiv(myNPR.genres); 
-  }
-}
-
-function updateContentDiv(content){
-  console.info('show content div');
-  $('#content').text(JSON.stringify(content)); 
-}
+  myNPR.loadTopicsAndGenres();
+  myNPR.loadTags();
+});
 
 (function(app){
-  a = app;
-  a.topics = null;
-  a.genres = null;
-  a.tags = null;
-  a.showTopics = showTopics;
-  a.showGenres = showGenres;
-  a.showTags = showTags;
+  app.topics = null;
+  app.genres = null;
+  app.tags = null;
+  
+  var uri = {
+    list : 'http://api.npr.org/list'
+  },
+  types = {
+    topics : {text: 'topics', id: 3002},
+    genres : {text: 'genres', id: 3018},
+    tags : {text: 'tags', id: 3024}
+  },
+  topicsRequestData = {
+    type  : types.topics.text,
+    uri   : uri.list,
+    data  : {
+      id  : types.topics.id
+    }
+  },
+  genresRequestData = {
+    type  : types.genres.text,
+    uri   : uri.list,
+    data  : {
+      id  : types.genres.id
+    }
+  }
+  tagsRequestData = {
+    type  : types.tags.text,
+    uri   : uri.list,
+    data  : {
+      id  : types.tags.id
+    }
+  };
+  
+  function loadTags(){
+    $.when(AJAX.requestList(tagsRequestData))
+      .then(function(tags){
+        app.tags = tags[0];
+        // Over 17k tags so it takes a while to load  =)
+        // The tags button is disabled until they're done loading
+        $('#tags').removeAttr('disabled');
+      });  
+  }
+  
+  function loadTopicsAndGenres(){
+    $.when(AJAX.requestList(topicsRequestData), AJAX.requestList(genresRequestData))
+      .then(function(topics, genres){
+        app.topics = topics[0];
+        app.genres = genres[0];
+      });  
+  }
+  
+  function showTopics(){
+    console.info('show topics');
+    if(!app.topics)
+    {
+      $.when(AJAX.requestList(topicsRequestData))
+        .then(function(data){app.topics = data; updateContentDiv(app.topics);});
+    }
+    else {
+      updateContentDiv(app.topics);
+    }
+  }
+  
+  function showTags(){
+    console.info('show tags');
+    if(!app.tags)
+    {
+      $.when(AJAX.requestList(tagsRequestData))
+        .then(function(data){
+          app.tags = data; 
+          updateContentDiv(app.tags);
+        });
+    }
+    else {
+      updateContentDiv(app.tags);
+    }
+  }
+  
+  function showGenres(){
+    console.info('show genres');
+    if(!app.genres)
+    {
+      $.when(AJAX.requestList(genresRequestData))
+        .then(function(data){
+          app.genres = data; 
+          updateContentDiv(app.genres);
+        });
+    }
+    else {
+      updateContentDiv(app.genres); 
+    }
+  }
+  
+  function updateContentDiv(content){
+    console.info('show content div');
+    $('#content').text(JSON.stringify(content)); 
+  }
+  
+  app.showTopics = showTopics;
+  app.showGenres = showGenres;
+  app.showTags = showTags;
+  app.loadTopicsAndGenres = loadTopicsAndGenres;
+  app.loadTags = loadTags;
 })(myNPR || (myNPR = {}));
 
